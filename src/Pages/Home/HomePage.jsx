93322ce9a369b01c7523ec5b58f848b56e9562e8
mohammad-rammal <mohammad.rammal@hotmail.com>
2025-03-26 00:10:31 +0200

    Home Page-NavBar and Slider

---
+++ src/Pages/Home/HomePage.jsx
@@ -0,0 +1,14 @@
+import NavBarLogin from "../../Components/utilities/NavBarLogin"
+import Slider from "../../Components/Home/Slider"
+
+const HomePage = () => {
+
+  return (
+    <div className="font">
+      <NavBarLogin />
+      <Slider />
+    </div>
+  )
+}
+
+export default HomePage
